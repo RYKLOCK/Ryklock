d33f66c806e9700e50f38e744c7ad57b082b9164
XandyDevz <73369639+XandyDevz@users.noreply.github.com>
2020-10-23 18:37:12 -0500

    Add files via upload

---
+++ Structures/ryk_command/ryk.cmd
@@ -0,0 +1,17 @@
+@echo off
+REM =========================================================
+    title RYK COMMAND
+REM =========================================================
+
+
+echo Ryk ERR! [No Loader]: Execute this command in a console [Error 501]
+echo loader.modules.cmdLoader;10:3
+
+
+echo Can't Find "ryk_modules" Folder! [Error 404]
+echo --------------------------------------------------------
+echo %user% is sub-ingreased! please re-boot!
+echo [Executed at %time% - %date%]
+echo More info: https://www.ryklock.org/errDocs/404+501/
+echo Please verify your project!
+pause > nul
